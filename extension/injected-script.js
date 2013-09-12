bb470fdf93c15f8b2a08a7639e109fdb1cc45a11
Vincent Scheib <scheib@gmail.com>
2013-09-12 14:51:54 -0700

    Working with hard coded keys.

--- extension/injected-script.js
+++ extension/injected-script.js
@@ -1,41 +1,48 @@
-"use strict"
+"use strict";
 
-//(function () {
+var keyStore = {};
 
-  var keyStore = {};
-  var lastMouse = undefined;
+var dispatchMouseEvent = function(target, typeString) {
+  var e = document.createEvent("MouseEvents");
+  e.initEvent(typeString, true, true);
+  target.dispatchEvent(e);
+}
 
-  var dispatchMouseEvent = function(target, typeString) {
-    var e = document.createEvent("MouseEvents");
-    e.initEvent(typeString, true, true);
-    target.dispatchEvent(e);
-  }
+var keydown = function(e) {
+  if (keyStore.hasOwnProperty(e.keyIdentifier)) {
+    var target = keyStore[e.keyIdentifier];
+    if (target === undefined) {
+      // Moderator may not have setup buttons yet, find them now.
+      findTargets();
+      target = keyStore[e.keyIdentifier]
 
-  var mousemove = function(e) { lastMouse = e; };
-
-  var keydown = function(e) {
-    if (e.ctrlKey) {
-      // Record click target.
-      console.log("Storing a key " + e.keyCode + " " + e.keyIdentifier);
-      keyStore[e.keyIdentifier] =
-          document.elementFromPoint(lastMouse.clientX, lastMouse.clientY);
-      e.preventDefault();
-    } else if (keyStore.hasOwnProperty(e.keyIdentifier)) {
-      // Send a synthetic click.
-      var target = keyStore[e.keyIdentifier];
-      console.log("Sending mousedown, mouseup, click, to ", target);
-      if (target) {
-        dispatchMouseEvent(target, "mouseover");
-        dispatchMouseEvent(target, "mousedown");
-        dispatchMouseEvent(target, "click");
-        dispatchMouseEvent(target, "mouseup");
-      }
-      e.preventDefault();
+      if (target === undefined)
+        console.errror("Can't find buttons to attach keyboard shortcuts to.");
     }
-  };
 
-  document.body.addEventListener("mousemove", mousemove);
+    console.log("Sending mouseover, mousedown, mouseup, click, to ", target);
+    if (target) {
+      dispatchMouseEvent(target, "mouseover");
+      dispatchMouseEvent(target, "mousedown");
+      dispatchMouseEvent(target, "click");
+      dispatchMouseEvent(target, "mouseup");
+    }
+    e.preventDefault();
+  }
+};
+
+var findTargets = function() {
+  var buttons = document.querySelectorAll(".featured .gwt-ToggleButton");
+  var yesButton = buttons[0] || undefined;
+  var noButton = buttons[1] || undefined;
+  keyStore["U+004A" /* j */] = yesButton;
+  keyStore["U+004B" /* k */] = noButton;
+}
+
+var init = function() {
   document.body.addEventListener("keydown", keydown);
-
+  findTargets();
   console.log("Keyboard shortcuts for Google Moderator.");
-//})();
+}
+
+window.onload = init;
