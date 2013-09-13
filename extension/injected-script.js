fb418a4829fd1c4c5cdb09d881bf61c19be4da75
Vincent Scheib <scheib@gmail.com>
2013-09-12 17:15:13 -0700

    Add visual hints.

--- extension/injected-script.js
+++ extension/injected-script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var keyStore = {};
+var retryTimeForFindTargets = 10;
 
 var dispatchMouseEvent = function(target, typeString) {
   var e = document.createEvent("MouseEvents");
@@ -9,42 +10,49 @@ var dispatchMouseEvent = function(target, typeString) {
 }
 
 var keydown = function(e) {
-  if (keyStore.hasOwnProperty(e.keyIdentifier)) {
-    var target = keyStore[e.keyIdentifier];
-    if (target === undefined) {
-      // Moderator may not have setup buttons yet, find them now.
-      findTargets();
-      target = keyStore[e.keyIdentifier]
-
-      if (target === undefined)
-        console.errror("Can't find buttons to attach keyboard shortcuts to.");
-    }
-
-    console.log("Sending mouseover, mousedown, mouseup, click, to ", target);
-    if (target) {
-      dispatchMouseEvent(target, "mouseover");
-      dispatchMouseEvent(target, "mousedown");
-      dispatchMouseEvent(target, "click");
-      dispatchMouseEvent(target, "mouseup");
-    }
+  var target = keyStore[e.keyIdentifier];
+  if (target) {
+    dispatchMouseEvent(target, "mouseover");
+    dispatchMouseEvent(target, "mousedown");
+    dispatchMouseEvent(target, "click");
+    dispatchMouseEvent(target, "mouseup");
     e.preventDefault();
   }
 };
+
+var insertHint = function (targetElement, hintText) {
+  var hintDiv = document.createElement("div");
+  hintDiv.classList.add("hintText");
+  hintDiv.innerText = hintText;
+  targetElement.insertBefore(hintDiv, targetElement.firstChild);
+}
+
+var bindKeyToTargetAndAddHint = function (targetElement, keyIdentifier, hintText) {
+  keyStore[keyIdentifier] = targetElement;
+  insertHint(targetElement, hintText);
+}
 
 var findTargets = function() {
   var buttons = document.querySelectorAll(".featured .gwt-ToggleButton");
   var yesButton = buttons[0];
   var noButton = buttons[1];
+  console.assert(buttons[2] === undefined, "Expected only 2 buttons.");
   var skipButton = document.querySelectorAll(".featured .goog-button-base")[0];
-  keyStore["U+004A" /* j */] = yesButton;
-  keyStore["U+004B" /* k */] = noButton;
-  keyStore["U+0020" /* space */] = skipButton;
+
+  if (!yesButton || !noButton || !skipButton) {
+    retryTimeForFindTargets = retryTimeForFindTargets * 2;
+    setTimeout(findTargets, retryTimeForFindTargets);
+    return;
+  }
+
+  bindKeyToTargetAndAddHint(yesButton, "U+004A", "j");
+  bindKeyToTargetAndAddHint(noButton, "U+004B", "k");
+  bindKeyToTargetAndAddHint(skipButton, "U+0020", "space");
 }
 
 var init = function() {
   document.body.addEventListener("keydown", keydown);
   findTargets();
-  console.log("Keyboard shortcuts for Google Moderator.");
 }
 
 window.onload = init;
